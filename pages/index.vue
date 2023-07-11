66020f26ecada96e513736420211a881b443ebf6
AnkitxxChici <ankuuuuuu23@gmail.com>
2023-07-11 08:57:00 +0530

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    <h2>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis fuga aperiam ipsum quia laborum sit nam, voluptas debitis omnis autem.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia facere sapiente, esse veniam rerum quaerat itaque sit velit amet est.</p>
+    </div>
+</template>
+
+<script setup>
+  
+</script>
+
+<style scoped>
+ h2{
+    margin-bottom: 25px;
+    font-size: 38px;
+ }
+ p {
+    margin: 26px 0;
+ }
+</style>
---
+++ pages/product/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in product">
+        <ProductCard :product="p" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  //  fetch the products
+  const { data: product } = await useFetch('https://fakestoreapi.com/products')
+
+  definePageMeta({
+    layout: "product",
+  })
+
+  useHead({
+    title: 'Nuxt Doja | Merch',
+    meta: [
+      { name: 'description', content: 'Nuxt 3 Merch'}
+    ]
+  })
+</script>
